5fa52a873f98d36ad2e23490b2d915517eb1cbdd
Vinicius Ghizi <vinighizi@gmail.com>
2023-05-10 14:09:16 -0300

    Project created

---
+++ src/main/java/com/dev/ghizi/dslist/entitites/Game.java
@@ -0,0 +1,32 @@
+package com.dev.ghizi.dslist.entitites;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "tb_game")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(name = "game_year")
+    private String year;
+    private String genre;
+    private String platform;
+    private String imgUrl;
+    private String ShortDescription;
+    private String longDescription;
+
+}
---
+++ src/test/java/com/dev/ghizi/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.ghizi.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
