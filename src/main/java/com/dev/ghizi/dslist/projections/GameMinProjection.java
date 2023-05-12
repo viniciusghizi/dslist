859a754d54f13a79545974f3c645e91756198256
Vinicius Ghizi <vinighizi@gmail.com>
2023-05-11 21:06:45 -0300

    Homolog OK

--- src/main/java/com/dev/ghizi/dslist/projections/GameMinProjection.java
+++ src/main/java/com/dev/ghizi/dslist/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.dev.ghizi.dslist.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
